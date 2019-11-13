22b2fb92b915a53276acf59ac0d916bab318458e
aqu1les <felipebarros.dev@gmail.com>
2019-11-13 00:17:02 -0300

    configured environments variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true
 };
+export const API_URL =
+  "http://faculdadez-env.qg5g5a2vp3.us-east-2.elasticbeanstalk.com";
+
+export const ASSETS_DIR =
+  "http://faculdadez-env.qg5g5a2vp3.us-east-2.elasticbeanstalk.com/js/assets";
